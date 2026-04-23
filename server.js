29876b5eef107217a8d24ef2cf9a29a0a4208e66
KhurramTaimoor <137267026+KhurramTaimoor@users.noreply.github.com>
2026-04-23 18:54:39 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 const productTypeRoutes      = require("./routes/productType");
 const categoryRoutes         = require("./routes/category");
 const productRoutes          = require("./routes/product");
